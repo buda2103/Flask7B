c6be6ba0bdebeefa85a1b283f82f51cdf160f2be
buda2103 <garciarobledodavid18@gmail.com>
2024-09-19 00:16:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-from flask import Flask
-
-from flask import render_template
-from flask import request
-
+from flask import Flask, render_template, request
 import pusher
 
 app = Flask(__name__)
@@ -11,23 +7,22 @@ app = Flask(__name__)
 def index():
     return render_template("app.html")
 
-
 @app.route("/Registrar")
 def alumnos():
     return render_template("Pago_Curso.html")
-    
 
 @app.route("/alumnos/guardar", methods=["POST"])
 def alumnosGuardar():
-    matricula      = request.form["txtMatriculaFA"]
+    matricula = request.form["txtMatriculaFA"]
     nombreapellido = request.form["txtNombreApellidoFA"]
-    return f"Matrícula {matricula} Nombre y Apellido {nombreapellido}"
+    return f"Matrícula {matricula}, Nombre y Apellido {nombreapellido}"
 
-def evento ():
- pusher_client = pusher.Pusher(
-app_id = "1867163"
-key = "2358693f2b619b363f59"
-secret = "880f60b50e86e4555c43"
-cluster = "us2"
-)
-pusher_client.trigger("my-channel", "my-event", {})
+def evento():
+    pusher_client = pusher.Pusher(
+        app_id="1867163",
+        key="2358693f2b619b363f59",
+        secret="880f60b50e86e4555c43",
+        cluster="us2",
+        ssl=True  # Add this to use SSL if needed
+    )
+    pusher_client.trigger("my-channel", "my-event", {})
