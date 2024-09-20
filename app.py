0fd529bd8b31cb6c18ec46ba88053528636079eb
buda2103 <garciarobledodavid18@gmail.com>
2024-09-20 00:28:27 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,59 @@
-from flask import Flask, render_template, request
+from flask import Flask
+
+from flask import render_template
+from flask import request
+
 import pusher
+
+import mysql.connector
+import datetime
+import pytz
+
+con = mysql.connector.connect(
+  host="185.232.14.52",
+  database="u760464709_tst_sep",
+  user="u760464709_tst_sep_usr",
+  password="dJ0CIAFF="
+)
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    con.close()
     return render_template("app.html")
 
-@app.route("/Registrar")
+@app.route("/alumnos")
 def alumnos():
-    return render_template("Pago-Curso.html")
+    con.close()
+    return render_template("alumnos.html")
 
 @app.route("/alumnos/guardar", methods=["POST"])
 def alumnosGuardar():
-    matricula = request.form["txtMatriculaFA"]
+    con.close()
+    matricula      = request.form["txtMatriculaFA"]
     nombreapellido = request.form["txtNombreApellidoFA"]
-    return f"Matrícula {matricula}, Nombre y Apellido {nombreapellido}"
+    return f"Matrícula {matricula} Nombre y Apellido {nombreapellido}"
 
-def evento():
+@app.route("/buscar")
+def buscar():
+    if not con.is_connected():
+        con.reconnect()
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM sensor_log")
+    
+    registros = cursor.fetchall()
+
+    return registros
+
+@app.route("/registrar", methods=["GET"])
+def registrar():
     pusher_client = pusher.Pusher(
-        app_id="1867163",
-        key="2358693f2b619b363f59",
-        secret="880f60b50e86e4555c43",
+        app_id="1714541",
+        key="cda1cc599395d699a2af",
+        secret="9e9c00fc36600060d9e2",
         cluster="us2",
-        ssl=True  # Add this to use SSL if needed
+        ssl=True
     )
-    pusher_client.trigger("my-channel", "my-event", {})
+
+    pusher_client.trigger("canalRegistrosTemperaturaHumedad", "registroTemperaturaHumedad", request.args)
